b88040d39c22889cbbfa10879b079adcd72df78b
Kenneth Hernandez <khken21@gmail.com>
2021-03-20 20:49:29 -0600

    Change test

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include "Code/IOManagement.h"
+#include "Code/ManualGPIO.h"
 
 //
 //uint8_t *buffer;
@@ -197,11 +198,21 @@
 //}
 
 int main()
-{
-    printf("test");
-    int array[] = {23,24,35};
-    set_gpio_list(array,3);
-    execute_monitoring();
+{pinMode(23,1);
+ int result =digitalRead(23);
+ int prev_result = result;
+ while (1){
+     result = digitalRead(23);
+     if (result!= prev_result) {
+         printf("changed to value %d", result);
+         prev_result = result;
+         sleep(0.1);
+     }
+    }
+//    printf("test");
+//    int array[] = {23,24,35};
+//    set_gpio_list(array,3);
+//    execute_monitoring();
 //    char array[20];
 //    int prueba = 1000;
 //    snprintf(array,10,"%d",prueba);
