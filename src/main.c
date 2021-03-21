cc5a09ef3756af9a7c3813a474158b55f3dbfe30
Jasson Rodriguez <jassonrm@icloud.com>
2021-03-21 01:59:24 -0600

    Fix print message

--- src/main.c
+++ src/main.c
@@ -3,15 +3,15 @@
 #include "ManualGPIO.h"
 
 int main(int argc, char const *argv[]) {
-	printf("Testing GPIO 3...");
-	pinMode(3, 0);
-	digitalWrite(3, 1);
+	printf("Testing GPIO 2...\n");
+	pinMode(2, 0);
+	digitalWrite(2, 1);
 	sleep(1);
-	digitalWrite(3, 0);
+	digitalWrite(2, 0);
 	sleep(1);
-	digitalWrite(3, 1);
+	digitalWrite(2, 1);
 	sleep(1);
-	digitalWrite(3, 0);
+	digitalWrite(2, 0);
 	sleep(1);
 	return 0;
 }
