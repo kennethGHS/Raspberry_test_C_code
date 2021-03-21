37a2e4ca399a0d4ab8aa0746f37ae4b87848193f
Jasson Rodriguez <jassonrm@icloud.com>
2021-03-21 00:21:55 -0600

    Add autotools

--- Makefile
+++
@@ -1,6 +0,0 @@
-CC = gcc
-SOURCE = main.c ./Code/IOManagement.c
-LIB = -lpigpio -lrt
-build:
-	gcc -o testeo $(SOURCE) -Wall -pthread $(LIB)
-
---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "ManualGPIO.h"
+
+int main(int argc, char const *argv[]) {
+	print("Testing GPIO 3...")
+	pinMode(3, 0);
+	digitalWrite(3, 1);
+	sleep(1);
+	digitalWrite(3, 0);
+	sleep(1);
+	digitalWrite(3, 1);
+	sleep(1);
+	digitalWrite(3, 0);
+	sleep(1);
+	return 0;
+}
